1c365f9f1035d4d821a3e68581bb18ab1acbe722
liuningning <1754102225@qq.com>
2020-11-13 18:09:16 +0800

    添加

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import axios from 'axios'
+import VueAxios from 'vue-axios'
 import util from './libs/util'
 import App from './App'
 import router from './router'
@@ -9,9 +10,10 @@ import 'view-design/dist/styles/iview.css';
 Vue.use(ViewUI);
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
-Vue.http = Vue.prototype.$http = axios;
+// Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
 Vue.prototype.$util = util;
+Vue.use(VueAxios, axios)
 
 /* eslint-disable no-new */
 new Vue({
